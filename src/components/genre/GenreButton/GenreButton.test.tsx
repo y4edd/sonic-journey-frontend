70d87b04041480ed33fb819b76af9a470f06d660
y4edd <yaeyuu0116@icloud.com>
2025-04-04 17:52:10 +0900

    fix: 通らないテストの修正

--- src/components/genre/GenreButton/GenreButton.test.tsx
+++ src/components/genre/GenreButton/GenreButton.test.tsx
@@ -3,7 +3,7 @@ import GenreButton from "./GenreButton";
 
 describe("GenreButtonコンポーネントの単体テスト", () => {
   const mockSetSelectGenre = jest.fn();
-  const mockData = { id: 0, name: "すべて", picture: "allGenre.png" };
+  const mockData = { id: 0, name: "すべて", picture: "allGenre.png", picture_xl: "allGenre.png" };
   test("レンダリングが適切に行われていること", () => {
     render(<GenreButton genre={mockData} selectGenre={1} setSelectGenre={mockSetSelectGenre} />);
     expect(screen.getByRole("button")).toBeInTheDocument();
